6683d08ec6c2725c26e33127bc341d6fb164e062
Gazetdinov Evgeniy <yngazetd@mts.ru>
2023-10-11 16:59:45 +0300

    добавление базового апи для просмотра и добавления статей

--- articles/views.py
+++ articles/views.py
@@ -1,14 +1,34 @@
-from django.shortcuts import render
-from articles.services.main_services import MainActicleService
+from rest_framework import generics
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-service = MainActicleService()
+from articles.serializer import ArticleSerializer
+from articles.models import Articles
 
-def main_template_with_articles(request):
-    all_acticles = service.get_all_articles()
-    return render(request, 'index.html', {'articles': all_acticles, 'menu': service.menu,
-                                          'title': 'Главная страница'})
 
-def main_template_get_article_by_id(request, pk):
-    article = service.get_all_articles()
-    return render(request, 'index.html', {'articles': article, 'menu': service.menu,
-                                          'title': article.title})
+class ArticleAPIView(APIView):
+    """
+    A view that returns a templated HTML representation of a given user.
+    """
+
+    def get(self, request, *args, **kwargs):
+        articles = Articles.objects.all()
+        return Response({'articles': ArticleSerializer(articles, many=True).data})
+
+    def post(self, request, *args, **kwargs):
+        # TODO fix validate
+        serializer = ArticleSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'data': ArticleSerializer.data})
+
+    def put(self, request, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        if pk:
+            article = Articles.objects.filter(id=pk)
+            serializer = ArticleSerializer(instance=request.data, many=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+        return Response({'articles': ArticleSerializer.data})
+    # def api_response(self, request):
